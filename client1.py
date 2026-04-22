d24e9fc7e9f0b89664f88a53a822578c3d6b8a1c
cmy05 <3373546629@qq.com>
2026-04-22 08:22:50 +0800

    Return the response content

--- client1.py
+++ client1.py
@@ -11,3 +11,4 @@ def send_request(sock: socket.socket, command: str) -> str:
      resp_len_bytes = sock.recv(3)
      resp_len = int(resp_len_bytes.decode().strip())
      resp = sock.recv(resp_len - 3).decode().strip()
+     return resp
