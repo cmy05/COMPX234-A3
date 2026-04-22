8112d7d02745182fccb94e3838cad9ca20f8e851
cmy05 <3373546629@qq.com>
2026-04-22 08:19:29 +0800

    Concatenate the length prefix and command, and encode them into bytes

--- client1.py
+++ client1.py
@@ -4,3 +4,5 @@ import sys
 def send_request(sock: socket.socket, command: str) -> str:
      msg_len = len(command) + 3
      formatted_len = f"{msg_len:03d}"
+     full_msg = (formatted_len + command).encode()
+     
