4afb326fa43a91e7a0d74aeb767234799be265d2
cmy05 <3373546629@qq.com>
2026-04-22 08:20:02 +0800

    Send the complete message to the server

--- client1.py
+++ client1.py
@@ -5,4 +5,5 @@ def send_request(sock: socket.socket, command: str) -> str:
      msg_len = len(command) + 3
      formatted_len = f"{msg_len:03d}"
      full_msg = (formatted_len + command).encode()
-     
+
+     sock.sendall(full_msg)
