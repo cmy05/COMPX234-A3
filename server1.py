b263cca09fd7152f5003838f891e0ed4ed579913
cmy05 <3373546629@qq.com>
2026-04-22 10:54:21 +0800

    Bind to the specified port of all network cards

--- server1.py
+++ server1.py
@@ -111,3 +111,4 @@ class TupleSpace:
          ts = TupleSpace()
          server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
          server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+         server.bind(("0.0.0.0", port))
