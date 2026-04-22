6dd89789cc18460753e281cac88f10614fba6331
cmy05 <3373546629@qq.com>
2026-04-22 08:57:05 +0800

    Constructor function, initializing tuple space

--- server1.py
+++ server1.py
@@ -4,3 +4,13 @@ import time
 from typing import Dict 
 
 class TupleSpace:
+    def __init__(self):  
+        self.tuples: Dict[str, str] = {}  
+        self.lock = threading.Lock()  
+
+        self.total_connections = 0  
+        self.total_ops = 0  
+        self.read_ops = 0  
+        self.get_ops = 0  
+        self.put_ops = 0  
+        self.error_ops = 0
